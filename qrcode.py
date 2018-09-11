80804ea976af18d2218cda93baf0846d805267de
dehuy69 <dr.huy69@gmail.com>
2018-09-12 00:37:46 +0700

    debug

--- qrcode.py
+++ qrcode.py
@@ -1,27 +1,29 @@
-from __future__ import print_function
 import pyzbar.pyzbar as pyzbar
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import numpy as np
-import cv2
 import time
-
-# cap = cv2.VideoCapture()
 camera = PiCamera()
-priviouse = []
-while(True):
-  # ret, frame = cap.read()
-  
-  rawCapture = PiRGBArray(camera)
-  camera.capture(rawCapture, format="bgr")
-  frame = rawCapture.array
-  cv2.imshow('frame',frame)
-  decodedObjects = pyzbar.decode(frame)
-  if decodedObjects != []:      
-    for obj in decodedObjects:
-      if obj.data != priviouse:
-        print('Type : ', obj.type)
-        print('Data : ', obj.data,'\n')
-        priviouse = obj.data
-  if cv2.waitKey(1) & 0xFF == ord('q'):
+camera.resolution = (800, 600)
+rawCapture = PiRGBArray(camera)
+privious_data = []
+privious_time = 0
+camera.start_preview(fullscreen=False, window=(100,20,800,600))
+for frame in camera.capture_continuous(rawCapture, format="bgr",
+                                      use_video_port=True):
+  try:
+    frame = rawCapture.array
+    decodedObjects = pyzbar.decode(frame)
+    if decodedObjects != []:
+      wait_time = time.time() - privious_time
+      for obj in decodedObjects:
+        if obj.data != privious_data or wait_time > 5:
+          print('Type : ', obj.type)
+          print('Data : ', obj.data,'\n')
+          privious_data = obj.data
+          privious_time = time.time()
+    rawCapture.truncate(0)
+  except:
+    camera.stop_preview()
     break
+  
--- testcam.py
+++ testcam.py
@@ -6,15 +6,18 @@ import cv2
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
+camera.resolution = (800, 600)
 rawCapture = PiRGBArray(camera)
 
 # allow the camera to warmup
 time.sleep(0.1)
 
 # grab an image from the camera
-camera.capture_continuous(rawCapture, format="bgr")
-image = rawCapture.array
-
+for frame in camera.capture_continuous(rawCapture, format="bgr",
+                                       use_video_port=True):
+    image = frame.array
 # display the image on screen and wait for a keypress
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+    cv2.imshow("Image", image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    rawCapture.truncate(0)
